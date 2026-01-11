a1ce281a277ca73026df5c15b37494c7f21217c2
mukesh1352 <mukeshnarravula14@gmail.com>
2026-01-11 14:11:32 +0530

    feat(backend):added the backend spring boot setup

---
+++ ledgerpay-backend/src/test/java/com/ledgerpay/ledgerpay_backend/LedgerpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledgerpay.ledgerpay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
